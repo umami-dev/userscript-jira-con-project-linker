958780d9ff2dabc148d2a363e75325fd4e9d83cd
Shangwei Tsai <83455523+t-shangwei@users.noreply.github.com>
2024-01-19 18:09:23 +0900

    Update userscript-jira-con-project-linker.user.js

--- userscript-jira-con-project-linker.user.js
+++ userscript-jira-con-project-linker.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Tripla CON project linker
 // @namespace    http://tampermonkey.net/
-// @version      2024-01-19.3
+// @version      2024-01-19.4
 // @description  try to take over the world!
 // @author       Shangwei Tsai
 // @match        https://github.com/umami-dev/*
-// @downloadURL  https://raw.githubusercontent.com/umami-dev/userscript-jira-con-project-linker/main/userscript-jira-con-project-linker.js
-// @updateURL    https://raw.githubusercontent.com/umami-dev/userscript-jira-con-project-linker/main/userscript-jira-con-project-linker.js
+// @downloadURL  https://raw.githubusercontent.com/umami-dev/userscript-jira-con-project-linker/main/userscript-jira-con-project-linker.user.js
+// @updateURL    https://raw.githubusercontent.com/umami-dev/userscript-jira-con-project-linker/main/userscript-jira-con-project-linker.user.js
 // @icon         https://tripla.jp/favicon.ico
 // @grant        none
 // ==/UserScript==
