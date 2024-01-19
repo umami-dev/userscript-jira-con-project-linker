140877f69643ebb362bd5abb5fad7137d40ab85c
Shangwei Tsai <83455523+t-shangwei@users.noreply.github.com>
2024-01-19 17:57:25 +0900

    Update userscript-jira-con-project-linker.js

--- userscript-jira-con-project-linker.js
+++ userscript-jira-con-project-linker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tripla CON project linker
 // @namespace    http://tampermonkey.net/
-// @version      2024-01-19
+// @version      2024-01-19.2
 // @description  try to take over the world!
 // @author       Shangwei Tsai
 // @match        https://github.com/umami-dev/*
@@ -11,7 +11,14 @@
 
 (function() {
     'use strict';
-    document.querySelectorAll('.commit-title, .js-issue-title').forEach((elm) => {
-        elm.innerHTML =  elm.innerHTML.replaceAll(/CON-\d+/ig, `<a href='https://umami-me.atlassian.net/browse/$&' target='_blank'>$&</a>`)
-    })
+    const transform = () => {
+        document.querySelectorAll('.commit-title, .js-issue-title').forEach((elm) => {
+            if(!elm.dataset.triplaConProjectLinkTransformed) {
+                elm.innerHTML =  elm.innerHTML.replaceAll(/CON-\d+/ig, `<a href='https://umami-me.atlassian.net/browse/$&' target='_blank'>$&</a>`)
+                elm.dataset.triplaConProjectLinkTransformed = 'done'
+            }
+        })
+    }
+    setInterval(transform, 2000) // for SPA
+
 })();
